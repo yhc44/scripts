5bd60559d2288f9f801af7a83f51e10b11069b85
Yann Heeser <yannheeser@Yanns-iMac.local>
2019-07-19 03:24:09 +0200

    Bugfix script

--- json2env.sh
+++ json2env.sh
@@ -2,5 +2,5 @@
 # e.g userId: 1337 ==> USERID=1337
 while read line
 do
-  echo $line | awk -F: '{if ($1!="{" && $1!="}") print toupper($1)"="$2}' | sed 's/"//g' | sed 's/,//g' | sed 's/= /=/g'
+  echo $line | awk '{if ($1!="{" && $1!="}") print toupper($1)"="$2}' | sed 's/"//g' | sed 's/,//g' | sed 's/:=/=/g'
 done
