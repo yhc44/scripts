5519df5e92870b523cc13d9ff7a07c138d6c410e
Yann Heeser <39420253+yhc44@users.noreply.github.com>
2019-07-19 02:28:28 +0200

    Change for cat input

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 # Script for converting json to env
 # e.g userId: 1337 ==> USERID=1337
-curl -s https://jsonplaceholder.typicode.com/todos/1 | jq |
 while read line
 do
   echo $line | awk -F: '{if ($1!="{" && $1!="}") print toupper($1)"="$2}' | sed 's/"//g' | sed 's/,//g' | sed 's/= /=/g'
